e1f1c3585965175f9d46a6758dc566b6af966f2b
frank-mendez <frankmendezresources@gmail.com>
2026-03-22 20:39:58 +0800

    feat: register LineHeight extension in TipTap extensions

--- components/editor/extensions.ts
+++ components/editor/extensions.ts
@@ -2,6 +2,7 @@ import StarterKit from '@tiptap/starter-kit'
 import Image from '@tiptap/extension-image'
 import Link from '@tiptap/extension-link'
 import Placeholder from '@tiptap/extension-placeholder'
+import { LineHeight } from './line-height'
 
 export const extensions = [
   StarterKit.configure({
@@ -20,4 +21,5 @@ export const extensions = [
   Placeholder.configure({
     placeholder: 'Start writing your post...',
   }),
+  LineHeight,
 ]
